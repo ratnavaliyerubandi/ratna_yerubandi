796c5a434ee06bf0e66a851aac431e1448032b46
ratnavaliyerubandi <55006965+ratnavaliyerubandi@users.noreply.github.com>
2019-09-10 08:42:16 +0530

    Update App.java

--- Test Scenarios/Scenario3/backend-test-sample-master/src/main/java/com/noesisinformatica/test/App.java
+++ Test Scenarios/Scenario3/backend-test-sample-master/src/main/java/com/noesisinformatica/test/App.java
@@ -8,7 +8,13 @@ public class App {
 
     public static void main( String[] args )
     {
-        DataService dataService = new DataService();
+        DatabaseConnectionInfo iDatabaseConnectionInfo = new DatabaseConnectionInfo();
+        iDatabaseConnectionInfo.setDriverClass("org.hsqldb.jdbcDriver");
+        iDatabaseConnectionInfo.setUrl("jdbc:hsqldb:termdb");
+        iDatabaseConnectionInfo.setUsername("root");
+        iDatabaseConnectionInfo.setPassword("admin");
+
+        DataService dataService = new DataService(iDatabaseConnectionInfo);
         System.out.println( "Number of terms : " + dataService.getAllTerm().size());
         dataService.saveTerm("Term from app");
     }
